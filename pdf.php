9998b95d45c0becd332f7c35ae55cd2070c8c0fe
IsabelliPinho <108484363+IsabelliPinho@users.noreply.github.com>
2023-05-04 11:51:58 -0300

    Create pdf.php

--- pdf.php
+++ pdf.php
@@ -11,21 +11,32 @@ require 'CaracteresEspeciais.php';
 portugues8+portugues9+matematica6+matematica7+matematica8+matematica9+ciencias6+ciencias7+ciencias8+ciencias9+historia6+historia7+historia8+historia9+geografia6+geografia7+geografia8+geografia9+ingles6+ingles7+ingles8+ingles9+artes6+artes7
 +artes8+artes9+edfisica6+edfisica7+edfisica8+edfisica9)/32
 AS media */
-$sql=("SELECT nome_completo,media FROM aluno where curso = 'enfermagem' and concorrencia = '[publica]' and deficiencia = 'nenhuma' order by media desc limit 30");
-$nsql = ("SELECT nome_completo,media FROM aluno order by media desc limit 30 offset 30");
-$query = ("SELECT nome_completo,media from aluno where curso = 'enfermagem' and deficiencia <> 'nenhuma'order by media desc limit 2");
 
-$mysqll = ("SELECT nome_completo,media from aluno where curso = 'enfermagem'  order by media desc limit 10");
+//Classificados ampla concorrência (Pública)
+$sql=("SELECT nome_completo,media FROM aluno where curso = 'enfermagem' and concorrencia = '[publica]' and deficiencia = 'nenhuma' and bairro <> 'Venânciosi' order by media desc limit 10");
+$nsql = ("SELECT nome_completo,media FROM aluno where curso = 'enfermagem' and concorrencia = '[publica]' and deficiencia = 'nenhuma'  and bairro <> 'Venânciosi' order by media desc limit 5 offset 10");
 
+//Classificados com deficiência 
+$query = ("SELECT nome_completo,media from aluno where curso = 'enfermagem' and deficiencia <> 'nenhuma'  and bairro <> 'Venânciosi' order by media desc limit 2");
+$nquery = ("SELECT nome_completo,media from aluno where curso = 'enfermagem' and deficiencia <> 'nenhuma'  and bairro <> 'Venânciosi' order by media desc limit 5 offset 2");
+
+//Classificados por cota
+$mysqll = ("SELECT nome_completo,media from aluno where curso = 'enfermagem' and deficiencia = 'nenhuma' and bairro = 'Venânciosi' order by media desc limit 10");
+$nmysqll = ("SELECT nome_completo,media from aluno where curso = 'enfermagem' and deficiencia = 'nenhuma' and bairro = 'Venânciosi' order by media desc limit 5 offset 2");
+
+//Classificados ampla concorrência (Privada)
 $my = ("SELECT nome_completo,media from aluno where curso = 'enfermagem' and concorrencia = '[privada]'  order by media desc limit 10");
+$nmy = ("SELECT nome_completo,media from aluno where curso = 'enfermagem' and concorrencia = '[privada]'  order by media desc limit 5 offset 10");
 
-
-$buscar = mysqli_query($conn,$nsql);
 
 $busca = mysqli_query($conn,$sql);
+$nbusca = mysqli_query($conn,$nsql);
 $search =  mysqli_query($conn,$query);
+$nsearch = mysqli_query($conn,$nquery);
 $procurar =  mysqli_query($conn,$mysqll);
+$nprocurar =  mysqli_query($conn,$nmysqll);
 $ir =  mysqli_query($conn,$my);
+$nir =  mysqli_query($conn,$nmy);
 
 
 $pdf = new TextNormalizerFPDF(); 
@@ -33,7 +44,8 @@ $pdf->AddPage();
 
 //Fonte e dimensões dos títulos do pdf
 $pdf->SetFont('Arial','B',20);
-$pdf->Cell(140,10,('Enfermagem'),0,0,'C');
+$pdf->SetFillColor(219,223,239);
+$pdf->Cell(140,10,('EEEP MANOEL MANO'),0,0,'C');
 //Logo do Manoel Mano
 $pdf->Image('eeep.png',0,5,30,21,'PNG'); 
 $pdf->ln(6); 
@@ -43,11 +55,10 @@ $pdf->SetFont('Arial','B',16);
 
 $pdf->ln(20); 
 $pdf->SetFont('Arial','B',12);
-$pdf->Cell(170,7,'Classificados',1,0,'C'); 
+$pdf->Cell(170,7,'Classificados- Ampla Pública',1,0,'C','F'); 
 $pdf->ln(); 
 $pdf->Cell(120,7,'Nome',1,0,'C'); 
-$pdf->Cell(50,7,'Notas',1,0,'C'); 
-
+$pdf->Cell(50,7,'Média Geral',1,0,'C'); 
 $pdf->ln(); 
 
 while ($resultado=mysqli_fetch_array($busca)) {
@@ -57,35 +68,66 @@ while ($resultado=mysqli_fetch_array($busca)) {
 }
 $pdf->ln(10); 
 
-$pdf->Cell(170,7,'Nâo Classificados',1,0,'C');
+$pdf->Cell(170,7,'Nâo Classificados-Ampla Pública',1,0,'C','F');
 $pdf->ln(); 
 
-while ($resultado=mysqli_fetch_array($buscar)) {
+while ($resultado=mysqli_fetch_array($nbusca)) {
     // dimensões das células do pdf   
     $pdf->setFillColor(220,220,220);
     $pdf->Cell(120,7,$resultado['nome_completo'],1,0); 
-    $pdf->Cell(50,7,$resultado['media'],1,1);   
+    $pdf->Cell(50,7,$resultado['media'],1,1,);   
 }
 $pdf->ln(10); 
-$pdf->Cell(170,7,'Classificados com Deficiência',1,0,'C');
-$pdf->ln(); 
+
+$pdf->Cell(170,7,'Classificados com Deficiência',1,0,'C','F');
+$pdf->ln();
+$pdf->Cell(120,7,'Nome',1,0,'C'); 
+$pdf->Cell(50,7,'Média Geral',1,0,'C'); 
+$pdf->ln();  
 while ($resultado=mysqli_fetch_array($search)) {
     // dimensões das células do pdf   
     $pdf->Cell(120,7,$resultado['nome_completo'],1,0); 
     $pdf->Cell(50,7,$resultado['media'],1,1);   
 }
+$pdf->Cell(170,7,'Não Classificados com Deficiência',1,0,'C','F');
+$pdf->ln(); 
+while ($resultado=mysqli_fetch_array($nsearch)) {
+    // dimensões das células do pdf  
+    $pdf->Cell(120,7,$resultado['nome_completo'],1,0); 
+    $pdf->Cell(50,7,$resultado['media'],1,1);   
+}
 $pdf->ln(10); 
-$pdf->Cell(170,7,'Classificados por Território',1,0,'C');
+$pdf->Cell(170,7,'Classificados por Território',1,0,'C','F');
 $pdf->ln();
+$pdf->Cell(120,7,'Nome',1,0,'C'); 
+$pdf->Cell(50,7,'Média Geral',1,0,'C'); 
+$pdf->ln(); 
 while ($resultado=mysqli_fetch_array($procurar)) {
     // dimensões das células do pdf   
     $pdf->Cell(120,7,$resultado['nome_completo'],1,0); 
     $pdf->Cell(50,7,$resultado['media'],1,1);   
 }
-$pdf->ln(10); 
-$pdf->Cell(170,7,'Privada',1,0,'C');
+$pdf->Cell(170,7,'Não Classificados por Território',1,0,'C','F');
 $pdf->ln();
+while ($resultado=mysqli_fetch_array($nprocurar)) {
+    // dimensões das células do pdf   
+    $pdf->Cell(120,7,$resultado['nome_completo'],1,0); 
+    $pdf->Cell(50,7,$resultado['media'],1,1);   
+}
+$pdf->ln(10); 
+$pdf->Cell(170,7,'Privada',1,0,'C','F');
+$pdf->ln();
+$pdf->Cell(120,7,'Nome',1,0,'C'); 
+$pdf->Cell(50,7,'Média Geral',1,0,'C'); 
+$pdf->ln(); 
 while ($resultado=mysqli_fetch_array($ir)) {
+    // dimensões das células do pdf   
+    $pdf->Cell(120,7,$resultado['nome_completo'],1,0); 
+    $pdf->Cell(50,7,$resultado['media'],1,1);   
+}
+$pdf->Cell(170,7,'Não Classificados Privada',1,0,'C','F');
+$pdf->ln();
+while ($resultado=mysqli_fetch_array($nir)) {
     // dimensões das células do pdf   
     $pdf->Cell(120,7,$resultado['nome_completo'],1,0); 
     $pdf->Cell(50,7,$resultado['media'],1,1);   
